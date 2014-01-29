f6af45abdbeed5f7fd76e0da3d0683c985c8e6a9
ychartois <yannickchartois@wanadoo.fr>
2014-01-29 19:38:47 +0100

    constructor by default example

---
+++ src/main/java/bzh/ys/javabyexample/constructor/ConstructorExample.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bzh.ys.javabyexample.constructor;
+
+/**
+ *
+ * @author yannig
+ */
+public class ConstructorExample {
+    
+    public ConstructorExample( String toto ) {
+        super();
+    }
+}
---
+++ src/main/java/bzh/ys/javabyexample/constructor/ConstructorExampleChild.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bzh.ys.javabyexample.constructor;
+
+/**
+ *
+ * @author yannig
+ */
+public class ConstructorExampleChild extends ConstructorExample {
+    
+    public ConstructorExampleChild() {
+        // super() <- doesn't works, we have to define the constructor, the default one can't work !
+        super("hello");
+    }
+    
+    void print(){
+        System.out.println("Hello World");
+    }
+    
+    public static void main(String[] args) {
+        ConstructorExampleChild example = new ConstructorExampleChild();
+        example.print();
+    }
+}
