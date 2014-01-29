d48734ecff30dd676ad933df8321530c1eda1217
ychartois <yannickchartois@wanadoo.fr>
2014-01-29 21:23:09 +0100

    example pass by value and pass by copy

---
+++ src/main/java/bzh/ys/javabyexample/constructor/passbyvalue/A.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bzh.ys.javabyexample.constructor.passbyvalue;
+
+/**
+ *
+ * @author yannig
+ */
+public class A {
+
+    int a = 1;
+    int b = 2;
+    
+    void swapValue( int a, int b ){
+        int x = a;
+        a = b;
+        b = x;
+    }
+    
+    void swapValue( B swap ){
+        int x = swap.a;
+        swap.a = swap.b;
+        swap.b = x;
+    }
+    
+    void swapValueAndErase( B swap ){
+        // swap
+        swapValue(swap);
+        
+        // and erase
+        swap = new B(); // <-- create a new ref, but the object is already modified !
+    }
+    
+    B swapValueAndEraseForReal( B swap ){
+        // swap
+        swapValue(swap);
+        
+        // and erase
+        swap = new B(); // <-- create a new ref, but the object is already modified !
+        
+        return swap;
+    }
+    
+    public static void main(String[] args) {
+        // swap primitive
+        A test = new A();
+        test.swapValue( test.a, test.b);
+        System.out.println("a: " + test.a + ", b: " + test.b);
+        
+        // swap object
+        B test2 = new B();
+        test.swapValue( test2 );
+        System.out.println( test2 );
+        
+        // swap and erase Object
+        B test3 = new B();
+        test.swapValueAndErase( test3 );
+        System.out.println( test3 );
+        
+        // swap and erase Object for real
+        B test4 = new B();
+        test4 = test.swapValueAndEraseForReal( test4 );
+        System.out.println( test4 );
+        
+        System.gc();
+        
+    }
+}
---
+++ src/main/java/bzh/ys/javabyexample/constructor/passbyvalue/B.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bzh.ys.javabyexample.constructor.passbyvalue;
+
+/**
+ *
+ * @author yannig
+ */
+public class B {
+    int a = 1;
+    int b = 2;
+    
+    @Override
+    public String toString() {
+        return "a: " + a + ", b: " + b;
+    }
+    
+}
