f2b3409582486f2186c8ae37bc5c0cb0d5a52a9e
ychartois <yannickchartois@wanadoo.fr>
2014-01-29 21:23:39 +0100

    modification of package name

--- src/main/java/bzh/ys/javabyexample/passbyvalue/A.java
+++ src/main/java/bzh/ys/javabyexample/passbyvalue/A.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package bzh.ys.javabyexample.constructor.passbyvalue;
+package bzh.ys.javabyexample.passbyvalue;
 
 /**
  *
