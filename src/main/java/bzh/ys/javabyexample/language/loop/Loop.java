9eb87c2a761d8ec7f3affdd0117903063dac77a6
ychartois <yannickchartois@wanadoo.fr>
2014-02-12 22:52:38 +0100

    loop examples

---
+++ src/main/java/bzh/ys/javabyexample/language/loop/Loop.java
@@ -0,0 +1,80 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bzh.ys.javabyexample.language.loop;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ *
+ * @author yannig
+ */
+public class Loop {
+    
+    static final List<String> test;
+    
+    static {
+        test = new ArrayList();
+        test.add("foo");
+        test.add("bar");
+    }
+    
+    public static void whileLoop() {
+        // Old style
+        Iterator<String> it = test.iterator();
+        while (it.hasNext()) {
+            String val = it.next();
+            System.out.println(val);
+        }
+    }
+    
+    public static void foriLoop() {
+        // For i loop
+        for (int i = 0; i < test.size(); i++) {
+            String val = test.get(i);
+            System.out.println(val);
+        }
+    }
+    
+    public static void forEachLoop() {
+        for (String val : test) {
+            System.out.println(val);
+        }
+    }
+    
+    public static void forContinue() {
+        
+        for (String val : test) {
+            if ( "foo".equals(val) ) {
+                continue;
+            }
+            System.out.println("Continue value: " + val);
+        }
+        
+    }
+    
+    public static void forBreak() {
+        
+        for (String val : test) {
+            if ( "foo".equals(val) )
+                break;
+            System.out.println("Break value: " + val);
+        }
+        
+    }
+   
+    
+    public static void main(String[] args) {
+        whileLoop();
+        foriLoop();
+        forEachLoop();
+        forContinue();
+    }
+    
+    
+    
+}
